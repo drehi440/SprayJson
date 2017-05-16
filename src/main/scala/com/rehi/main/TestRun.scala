b0dce8ebaff18b034df0795c1d4884fd76793476
DRehi <DRehi@HIGURL61740.ad.harman.com>
2017-05-16 14:37:45 +0530

    @minor fixes, proper parameterised

--- src/main/scala/com/rehi/main/TestRun.scala
+++ src/main/scala/com/rehi/main/TestRun.scala
@@ -3,13 +3,15 @@ package com.rehi.main
 object TestRun {
   
   def main(args: Array[String]): Unit = {
-    println("yeaa")
+    println("@Main SprayJson")
     
     import com.rehi.main.MyJsonProtocol._
     import spray.json._
     
     val json = Bucket("123",22,Value(3)).toJson
     
+    val value = json.asJsObject.fields.get("avg")
+    println(value)
     println(json)
     
   }
